b16406de5910978d5d85267e0a79aa283e31e873
Victor Shcherb <vics001@gmail.com>
2011-08-02 21:13:53 +0200

    Clean libraries

--- recompile.sh
+++
@@ -1,6 +0,0 @@
-rm -rv src/com/anvisics/jleveldb/ext
-mkdir src/com/anvisics/jleveldb/ext
-swig -c++ -java -package com.anvisics.jleveldb.ext -outdir src/com/anvisics/jleveldb/ext -o build/db_wrap.cpp db.i;
-g++ -c -fpic build/db_wrap.cpp -I/usr/lib/jvm/java-6-openjdk/include/ -Ibuild/leveldb/include/ -o build/obj/db_wrap.o;
-g++ -shared build/obj/*.o -o src/jleveldb-linux-x86.lib;
-
